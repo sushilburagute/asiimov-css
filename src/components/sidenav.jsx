f808f673f24e049c3e12e58771f24fb1271faccf
sushilburagute <codetastic101@gmail.com>
2021-04-03 10:57:08 +0530

    Code Clean up

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -1,27 +1,65 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const sidebarItems = [
+  {
+    name: "Avatars",
+  },
+  {
+    name: "Alert",
+  },
+  {
+    name: "Badges",
+  },
+  {
+    name: "Buttons",
+  },
+  {
+    name: "Cards",
+  },
+  {
+    name: "Image",
+  },
+  {
+    name: "Input",
+  },
+  {
+    name: "Lists",
+  },
+  {
+    name: "Modal",
+  },
+  {
+    name: "Typograhy",
+  },
+  {
+    name: "Toast Notifcations",
+  },
+];
+
 export const Sidenav = () => {
   return (
-    <div className="sidenav">
-      <h3 className="sidenav-heading">Getting Started</h3>
-      <ul className="list">
-        <li className="sidenav-stacked-list-selected">Installation</li>
-        <li className="sidenav-stacked-list">What's new</li>
-      </ul>
-      <h3 className="sidenav-heading">Components</h3>
-      <ul className="list">
-        <li className="sidenav-stacked-list">Avatars</li>
-        <li className="sidenav-stacked-list">Alert</li>
-        <li className="sidenav-stacked-list">Badges</li>
-        <li className="sidenav-stacked-list">Buttons</li>
-        <li className="sidenav-stacked-list">Cards</li>
-        <li className="sidenav-stacked-list">Image</li>
-        <li className="sidenav-stacked-list">Input</li>
-        <li className="sidenav-stacked-list">Lists</li>
-        <li className="sidenav-stacked-list">Modal</li>
-        <li className="sidenav-stacked-list">Typograhy</li>
-        <li className="sidenav-stacked-list">Toast Notifcations</li>
-      </ul>
-      <h3 className="sidenav-heading">🚀 Made by Sushil Buragute</h3>
-      <h5 className="sidenav-heading">Got some feedback? Tweet me!</h5>
-    </div>
+    <Router>
+      <div className="sidenav">
+        <h3 className="sidenav-heading">Getting Started</h3>
+        <ul className="list">
+          <li className="sidenav-stacked-list-selected">
+            <Link to="/documentation/installation">Installation</Link>
+          </li>
+          <li className="sidenav-stacked-list">
+            <Link to="/documentation/whats-new">What's new</Link>
+          </li>
+        </ul>
+        <h3 className="sidenav-heading">Components</h3>
+        <ul className="list">
+          {sidebarItems.map((items, key) => (
+            <li className="sidenav-stacked-list" key={items.name}>
+              {items.name}
+            </li>
+          ))}
+        </ul>
+        <h3 className="sidenav-heading">🚀 Made by Sushil Buragute</h3>
+        <h5 className="sidenav-heading">Got some feedback? Tweet me!</h5>
+      </div>
+    </Router>
   );
 };
--- src/pages/documentation.jsx
+++ src/pages/documentation.jsx
@@ -1,18 +1,76 @@
 import { Content } from "./../components/content";
 import { Sidenav } from "./../components/sidenav";
 import { HeaderDocs } from "./../components/header-docs";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+const routes = [
+  {
+    path: "/documentation",
+    exact: true,
+    content: () => <h1>Home of docs in Content</h1>,
+  },
+  {
+    path: "/documentation/installation",
+    exact: true,
+    content: () => <h1>Home of installation in Content</h1>,
+  },
+  {
+    path: "/documentation/whats-new",
+    exact: true,
+    content: () => <h1>Home of whats-new in Content</h1>,
+  },
+];
+
+const DocHome = () => {
+  return <h1>Home of docs in Content</h1>;
+};
+
+const Installation = () => {
+  return <h1>Home of Installation in Content</h1>;
+};
+
+const WhatsNew = () => {
+  return <h1>Home of WhatsNew in Content</h1>;
+};
 
 export const Documentation = () => {
   return (
-    <>
-      <Router>
-        <HeaderDocs />
-        <div className="body">
-          <Sidenav />
-          <Content />
+    <Router>
+      <HeaderDocs />
+      <div className="body">
+        <Sidenav />
+        {/* <Content /> */}
+        <div className="content">
+          <Switch>
+            {/* <>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  children={<route.content />}
+                />
+              ))}
+            </> */}
+
+            <Route path="/documentation/installation">
+              <Installation />
+            </Route>
+            <Route path="/documentation/whats-new">
+              <WhatsNew />
+            </Route>
+            <Route exact path="/documentation">
+              <DocHome />
+            </Route>
+          </Switch>
         </div>
-      </Router>
-    </>
+      </div>
+    </Router>
   );
 };
